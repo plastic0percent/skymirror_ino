42c5c61a17af8156456e53b13aa02a859fe6776f
Zhang Maiyun <myzhang1029@hotmail.com>
2021-02-25 23:29:47 +0800

    Add cmd spec

--- skymirror/skymirror.ino
+++ skymirror/skymirror.ino
@@ -1,7 +1,7 @@
+#include "Adafruit_MPU6050.h"
 #include "Beeper.h"
 #include "Pressure.h"
 #include "TinyGPS++.h"
-#include "Adafruit_MPU6050.h"
 
 // Constants
 // GPS on Serial2
@@ -27,7 +27,11 @@ Adafruit_MPU6050 mpu;
 // GPS Decoder
 TinyGPSPlus gps;
 
-void log(const char *message) { SerialC.println(message); }
+void log(const char *message)
+{
+    SerialC.println(message);
+    SerialB.println(message);
+}
 
 // Initialize bluetooth
 void init_bluetooth()
@@ -60,8 +64,42 @@ void setup()
     // TODO: Camera, Motor
 }
 
+// Bluetooth commands:
+// - 0x01 $1: receive $1 frames from the camera
+// - 0x02   : receive current speed
+// - 0x03   : receive current depth
+// - 0x04   : receive current gps location
+// - 0x10 $1: set speed to $1 m/s
+// - 0x20 $1 $2: set direction to $i degrees N, cw if $2 is 0, ccw is $2 is 1
+// - 0x30 $1: set depth to $1 m
+// - 0x40 $1: set fish repeller frequency to $1 Hz
+void exec_bluetooth_cmd()
+{
+    int cmd = SerialB.read();
+    switch (cmd)
+    {
+        case 0x01:
+        case 0x02:
+        case 0x03:
+        case 0x04:
+        case 0x10:
+        case 0x20:
+        case 0x30:
+        case 0x40:
+            log("Stub");
+            break;
+        default:
+            log("Unrecognized command received from bluetooth");
+    }
+}
+
 void loop()
 {
     log("Loop");
+    fish_repeller.beep(1000, 1.0);
+    if (SerialB.available())
+    {
+        exec_bluetooth_cmd();
+    }
     delay(9);
 }
